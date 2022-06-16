1721b8552e6059c5031c9c549649dd50f2b98a9c
RocioDelia1 <rociodelia8@gmail.com>
2022-06-16 15:44:39 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+
+<?php
+
+$pg = "inicio";
+?>
+
+
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/estilos.css">
+
+    <title>Proyectos</title>
+</head>
+
+<body id="inicio">
+    <?php include_once "header.php";?>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 mt-4 text-center div-pc">
+                <a href="proyectos.php"><img src="images/Workspace_2_SVG.svg" class="pc"></a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-6 my-4 mt-sm-5 text-center offset-sm-3 mb-3">
+                <div class="input-home">
+                    <p class="p-1">Bienvenid@ a mi sitio web</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-14 text-center">
+                <a href="proyectos.php" class="btn shadow">Conoce mis proyectos</a>
+            </div>
+        </div>
+    </main>
+    <footer class="container">
+        <div class="row mt-5 pb-3">
+            <div class="col-12 col-sm-3 text-center text-sm-start">
+                <a href="https://github.com/RocioDElia1" target="_blank" title="Github"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/rocio-d-elia-4775b6193/" target="_blank" title="Linkedin"><i
+                        class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0">
+                Sponsor <a href="https://depcsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
+            </div>
+            <div class="col-12 col-sm-3 text-sm-start  pt-2 p-sm-0">
+                <a href="mailto:rociodelia8@gmail.com">rociodelia8@gmail.com</a>
+            </div>
+        </div>
+    </footer>
+    <div class="whatsapp">
+        <a href="https://api.whatsapp.com/send?phone=541164422649" title="Whatsapp"><i class="fab fa-whatsapp"></i></a>
+    </div>
+    <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script></body>
+
+</html>
